ae5b4c4d48cdee897d4d735ac75124fd151c8155
santhosh <144583697+santhoshofcl@users.noreply.github.com>
2024-04-25 09:07:32 +0530

    Create Object Detection using OpenCV and Python.py

---
+++ Object Detection using OpenCV and Python/Object Detection using OpenCV and Python.py
@@ -0,0 +1,16 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+image = cv2.imread('photo.png')
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow('Detected Faces', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
